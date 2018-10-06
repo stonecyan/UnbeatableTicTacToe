661874f1c47f8192278940b8131f8ab9e47ec380
Stone Yan <stoneyan1995@gmail.com>
2018-10-06 08:21:11 -0700

    Initial Commit - Created icon selection, board, initial turn logic, and win condition

---
+++ TicTacToe/TicTacToe/Game.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Game:Rules
+    {
+        static void Main(string[] args)
+        {
+            string[,] board = new string[3, 3];
+            Console.WriteLine("Can you beat the computer in Tic Tac Toe?");
+            //Picking X or O
+            bool checkAnswer = false;
+            string human = null;
+            string cpu = null;
+            int turn = 0;
+            while (!checkAnswer)
+            {
+                Console.WriteLine("X or O? X goes first.");
+                string input = Console.ReadLine().ToLower();
+                if (input=="x")
+                {
+                    human = "X";
+                    cpu = "O";
+                    checkAnswer = true;
+                }
+                if (input=="o")
+                {
+                    human = "O";
+                    cpu = "X";
+                    turn = 1;
+                    checkAnswer = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid seletion.");
+                }
+
+            }
+
+            //Starting Game
+            Console.WriteLine("These are the cells to choose from:");
+            Console.WriteLine();
+            PrintCell();
+            Startgame(board);
+            while(true)
+            {
+                Print(board);
+
+                //Picking a cell
+                bool validAnswer = false;
+                int pickedCell = 0;
+                while (!validAnswer)
+                {
+                    Console.WriteLine("Enter the cell you want to play: ");
+                    validAnswer = int.TryParse(Console.ReadLine(), out pickedCell);
+                    if (!validAnswer) Console.WriteLine("Please enter a valid integer.");
+                    if (pickedCell < 1 || pickedCell > 9)
+                    {
+                        validAnswer = false;
+                        Console.WriteLine("Please enter an integer from 1-9.");
+                    }
+                }
+
+                var selected = SelCell(pickedCell);
+
+                string currentPlayer;
+
+                if (turn%2==0)
+                {
+                    currentPlayer = human;
+                }
+                else
+                {
+                    currentPlayer = cpu;
+                }
+
+                board[selected.row, selected.col] = currentPlayer;
+                turn++;
+
+            }
+            
+
+
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Rules.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Rules
+    {
+        //Print empty board
+        public static void Startgame(string[,] board)
+        {
+            for(int row=0; row<3; row++)
+            {
+                for(int col=0;col<3;col++)
+                {
+                    board[row, col] = " ";
+                }
+            }
+        }
+
+        //Print current board
+        public static void Print(string[,] board)
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (col == 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(board[row, col]);
+                    if (col == 0 || col == 1)
+                    {
+                        Console.Write(" | ");
+                    }
+                }
+                Console.WriteLine();
+                if (row == 0 || row == 1)
+                    Console.WriteLine("-----------");
+            }
+            Console.ReadLine();
+        }
+
+        //Print example board
+        public static void PrintCell()
+        {
+            int c = 1;
+            string [,] cellboard = new string[3, 3];
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    string d = Convert.ToString(c);
+                    cellboard[row, col] = d;
+                    c++;
+                }
+            }
+
+            Print(cellboard);
+        }
+
+        //Convert picked cell into row,col combination for board
+        public static (int row, int col) SelCell(int pickedCell)
+        {
+            int row=0;
+            int col=0;
+
+            if (pickedCell==1)
+            {
+                row = 0;
+                col = 0;
+            }
+            if (pickedCell == 2)
+            {
+                row = 0;
+                col = 1;
+            }
+            if (pickedCell == 3)
+            {
+                row = 0;
+                col = 2;
+            }
+            if (pickedCell == 4)
+            {
+                row = 1;
+                col = 0;
+            }
+            if (pickedCell == 5)
+            {
+                row = 1;
+                col = 1;
+            }
+            if (pickedCell == 6)
+            {
+                row = 1;
+                col = 2;
+            }
+            if (pickedCell == 7)
+            {
+                row = 2;
+                col = 0;
+            }
+            if (pickedCell == 8)
+            {
+                row = 2;
+                col = 1;
+            }
+            if (pickedCell == 9)
+            {
+                row = 2;
+                col = 2;
+            }
+
+            return (row, col);
+        }
+
+        public static CheckWin(string[,] board)
+        {
+            bool win;
+            if (board[0,0]==board[0,1] && board[0,1]==board[0,2])
+            {
+                win = true;
+            }
+            if (board[1,0]==board[1, 1] && board[1, 1] == board[1, 2])
+            {
+                win = true;
+            }
+            if (board[2, 0] == board[2, 1] && board[2,1] == board[2, 2])
+            {
+                win = true;
+            }
+            if (board[0, 0] == board[1, 0] && board[0, 1] == board[2, 0])
+            {
+                win = true;
+            }
+            if (board[0, 1] == board[1, 1] && board[0, 1] == board[2, 0])
+            {
+                win = true;
+            }
+            if (board[0, 2] == board[1, 2] && board[0, 2] == board[2, 2])
+            {
+                win = true;
+            }
+
+        }
+        
+    }
+}
